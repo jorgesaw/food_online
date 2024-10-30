3b74fa651176cac78fb4ec69688e5194dd7a182e
Jorgesaw <jorgesaw@gmail.com>
2024-10-30 19:32:19 -0300

    Vendor profile form + validation

--- vendor/views.py
+++ vendor/views.py
@@ -7,8 +7,8 @@ from django.db import IntegrityError
 # from menu.forms import CategoryForm, FoodItemForm
 # from orders.models import Order, OrderedFood
 import vendor
-# from .forms import VendorForm, OpeningHourForm
-# from accounts.forms import UserProfileForm
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
 
 from accounts.models import UserProfile
 from .models import Vendor #, OpeningHou
@@ -31,28 +31,28 @@ def vprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     vendor = get_object_or_404(Vendor, user=request.user)
 
-    # if request.method == 'POST':
-    #     profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
-    #     vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
-    #     if profile_form.is_valid() and vendor_form.is_valid():
-    #         profile_form.save()
-    #         vendor_form.save()
-    #         messages.success(request, 'Settings updated.')
-    #         return redirect('vprofile')
-    #     else:
-    #         print(profile_form.errors)
-    #         print(vendor_form.errors)
-    # else:
-    #     profile_form = UserProfileForm(instance = profile)
-    #     vendor_form = VendorForm(instance=vendor)
-    #
-    # context = {
-    #     'profile_form': profile_form,
-    #     'vendor_form': vendor_form,
-    #     'profile': profile,
-    #     'vendor': vendor,
-    # }
-    context = {}
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'Perfil actualizado.')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance = profile)
+        vendor_form = VendorForm(instance=vendor)
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+
     return render(request, 'vendor/vprofile.html', context)
 
 
